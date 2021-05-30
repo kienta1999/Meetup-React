3f6e302893457cdec3f2014401a15b493bbc27ad
kienta1999 <talekien1710@gmail.com>
2021-05-31 05:44:23 +0700

    React meetup app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 
-import AllMeetupsPage from "./pages/AllMeetups";
-import NewMeetupPage from "./pages/NewMeetup";
-import FavoritesPage from "./pages/Favorites";
-import MainNavigation from './components/layout/MainNavigation';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
-        <Route path="/" exact={true}>
+        <Route path='/' exact>
           <AllMeetupsPage />
         </Route>
-
-        <Route path="/new-meetup" exact>
+        <Route path='/new-meetup'>
           <NewMeetupPage />
         </Route>
-
-        <Route path="/favorites" exact>
+        <Route path='/favorites'>
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
